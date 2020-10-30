3ee63719939028f1ffaad93ede56aeb5abbb7908
bhargavmln <bhargavmln@gmail.com>
2020-10-30 17:18:21 +0530

    [bhargavmln]add:ability to find bowler with best bowling strike rate

--- src/main/java/com/capg/iplanalyser/IPL_LeagueAnalyser.java
+++ src/main/java/com/capg/iplanalyser/IPL_LeagueAnalyser.java
@@ -49,6 +49,14 @@ public class IPL_LeagueAnalyser {
 				.sorted(Comparator.comparing(Bowler::getAverage)).collect(Collectors.toList());
 		return toJson(sortedBowlerList);
 	}
+	
+	public String getMaximumBowlingStrikeRatesCricketers() {
+		List<Bowler> sortedBowlerList = bowlerList.stream()
+				.filter(n -> n.getStrikeRate()>0)
+				.sorted(Comparator.comparing(Bowler::getStrikeRate))
+				.collect(Collectors.toList());
+		return toJson(sortedBowlerList);
+	}
 
 	public <E> String toJson(List<E> list) {
 		return new Gson().toJson(list);
