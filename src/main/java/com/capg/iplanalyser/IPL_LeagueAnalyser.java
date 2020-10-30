39eb507ba99e43d904e30884aa683e92edb31c4a
bhargavmln <bhargavmln@gmail.com>
2020-10-30 17:05:06 +0530

    [bhargavmln]add:ability to find cricketer with best bowling average

--- src/main/java/com/capg/iplanalyser/IPL_LeagueAnalyser.java
+++ src/main/java/com/capg/iplanalyser/IPL_LeagueAnalyser.java
@@ -8,9 +8,13 @@ import com.google.gson.Gson;
 
 public class IPL_LeagueAnalyser {
 	private List<Batsman> batsmanList = null;
+	private List<Bowler> bowlerList = null;
 
 	public void loadBatsmanData(String PATH) throws IPLAnalyserException {
 		batsmanList = new CSVBatsman().loadBatsmanList(PATH);
+	}
+	public void loadBowlerData(String BOWLER_CSV_PATH) throws IPLAnalyserException {
+		bowlerList = new CSVBowler().loadBowlerList(BOWLER_CSV_PATH);
 	}
 
 	public String getBestBattingAveragesCricketers() {
@@ -19,25 +23,31 @@ public class IPL_LeagueAnalyser {
 				.collect(Collectors.toList());
 		return toJson(sortedStateBatsmanList);
 	}
-	
+
 	public String getBestStrikeRateCricketers() {
 		List<Batsman> sortedStateBatsmanList = batsmanList.stream()
 				.sorted(Comparator.comparing(Batsman::getStrikeRate).thenComparing(Batsman::getBoundries).reversed())
 				.collect(Collectors.toList());
 		return toJson(sortedStateBatsmanList);
 	}
-	
+
 	public String getMaximumBoundriesCricketers() {
 		List<Batsman> sortedStateBatsmanList = batsmanList.stream()
-				.sorted(Comparator.comparing(Batsman :: getBoundries).reversed()).collect(Collectors.toList());
+				.sorted(Comparator.comparing(Batsman::getBoundries).reversed()).collect(Collectors.toList());
 		return toJson(sortedStateBatsmanList);
 	}
-	
+
 	public String getMaximumRunsCricketers() {
 		List<Batsman> sortedStateBatsmanList = batsmanList.stream()
 				.sorted(Comparator.comparing(Batsman::getRunsScored).thenComparing(Batsman::getAverage).reversed())
 				.collect(Collectors.toList());
 		return toJson(sortedStateBatsmanList);
+	}
+
+	public String getMaximumBowlingAverageCricketers() {
+		List<Bowler> sortedBowlerList = bowlerList.stream().filter(n -> n.getAverage() > 0)
+				.sorted(Comparator.comparing(Bowler::getAverage)).collect(Collectors.toList());
+		return toJson(sortedBowlerList);
 	}
 
 	public <E> String toJson(List<E> list) {
--- src/test/java/com/capg/iplanalyser/IPL_LeagueAnalyserTest.java
+++ src/test/java/com/capg/iplanalyser/IPL_LeagueAnalyserTest.java
@@ -10,6 +10,7 @@ import com.google.gson.Gson;
 public class IPL_LeagueAnalyserTest {
 
 	private static final String BATSMAN_DATA_PATH = "./src/test/resources/BatsmanData.csv";
+	private static final String BOWLER_DATA_CSV_PATH = "./src/test/resources/BowlerData.csv";
 
 	IPL_LeagueAnalyser ipl_LeagueAnalyser = null;
 
@@ -18,6 +19,7 @@ public class IPL_LeagueAnalyserTest {
 		ipl_LeagueAnalyser = new IPL_LeagueAnalyser();
 		try {
 			ipl_LeagueAnalyser.loadBatsmanData(BATSMAN_DATA_PATH);
+			ipl_LeagueAnalyser.loadBowlerData(BOWLER_DATA_CSV_PATH);
 		} catch (IPLAnalyserException e) {
 		}
 	}
@@ -52,5 +54,13 @@ public class IPL_LeagueAnalyserTest {
 		Batsman[] sortedBatsmanArray = new Gson().fromJson(sortedBatsmanData, Batsman[].class);
 		System.out.println(sortedBatsmanArray[0].getName() + sortedBatsmanArray[0].getAverage());
 		assertEquals("David Warner", sortedBatsmanArray[0].getName());
+	}
+	
+	@Test
+	public void givenCSVFile_shouldReturn_CricketersWith_MaximumBowlingAverages() {
+		String sortedBowlerData = ipl_LeagueAnalyser.getMaximumBowlingAverageCricketers();
+		Bowler[] sortedBowlerArray = new Gson().fromJson(sortedBowlerData, Bowler[].class);
+		System.out.println(sortedBowlerArray[0].getName() + sortedBowlerArray[0].getAverage());
+		assertEquals("Anukul Roy", sortedBowlerArray[0].getName());
 	}
 }
